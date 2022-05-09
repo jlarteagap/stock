dac04fd3f7fa8c2775b75f83a04d98d1b84edc01
Jorge <jorgelarteagap@gmail.com>
2022-05-09 17:29:19 -0400

    [FIX] detect link

--- src/components/slide/DownloadApp.js
+++ src/components/slide/DownloadApp.js
@@ -1,33 +1,14 @@
 import React from 'react'
-import { isAndroid, isIOS } from 'react-device-detect'
+// import { isAndroid, isIOS } from 'react-device-detect'
 import useData from '../../hooks/useData'
 
 export const Download = () => {
   const { company } = useData()
 
-  let link = ''
-  let android = ''
-  let iOS = ''
-  if (company.apps.includes('playstore')) {
-    android = company.apps
-  } else if (company.apps.includes('appstore')) {
-    iOS = 'https://appstore.com'
-  } else {
-    link = company.apps
-  }
-
-  if (isAndroid) {
-    link = android
-  } else if (isIOS) {
-    link = iOS
-  } else {
-    link = company.apps
-  }
-
   return (
     <a
       className="slide__button button is-rounded is-large"
-      href={link}
+      href={company.apps}
       target="_blank"
       rel="noreferrer"
     >
